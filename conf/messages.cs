6d21d13a8c20f12b963313431ae864be0b3a2f71
Marek Suchánek <suchama4@fit.cvut.cz>
2017-05-23 16:48:24 +0200

    Czech translation

--- conf/messages.cs
+++ conf/messages.cs
@@ -1,6 +1,7 @@
 basic.search=Hledat
 basic.scan=Skenovat
 basic.things=Položky
+basic.thing=Položka
 basic.labels=Štítky
 basic.label=Štítek
 basic.create=Vytvořit
@@ -9,6 +10,8 @@ basic.atomic_thing=Položka
 basic.container=Kontejner
 basic.functional_whole=Funkční celek
 basic.error=Chyba
+basic.barcodes=Čárové kódy
+basic.barcode=Čárový kód
 
 view.index.text=Jednoduchá ale chytrá aplikace pro správu vlastních položek. Položkou může být fyzická věc skladovaná ve sklepě, stejně jako záznam o osobě nebo události. Model v aplikaci je natolik flexibilní, že umožňuje zachycení libovolných položek.
 
@@ -17,21 +20,58 @@ view.search.button=Hledej!
 view.search.barcode.examples=Příklady
 view.search.barcode_scan=Skenovat čárový kód
 view.search.barcode_404=Čárový kód není v aplikaci registrován
+view.search.barcode.found=Položka s čárovám kódem byla nalezena
 
 view.label.create.title=Vytvořit štítek
 view.label.index.title=Štítky
 view.label.created=Štítek byl vytvořen
 view.label.updated=Štítek byl aktualizován
 view.label.deleted=Štítek byl odstraněn
+view.label.description=Info
+view.label.things=Položky
+view.label.manage=Správa
 
 view.thing.index.title=Položky
 view.thing.create.atomic=Vytvořit samostatnou položku
 view.thing.create.container=Vytvořit kontejner
 view.thing.create.functional=Vytvořit funkční celek
+view.thing.basic=Základní
+view.thing.barcodes=Čárové kódy
+view.thing.labels=Štítky
+view.thing.parts=Části
+view.thing.wholes=Celky
+view.thing.manage=Správa
+view.thing.created=Položka byla vytvořena
+view.thing.updated=Položka byla aktualizována
+view.thing.not_updated=Položka nebyla aktualizována
+view.thing.deleted=Položka byla odstraněna
+view.thing.no_labels=Nejsou zatím přiřazeny žádné štítky!
+view.thing.no_barcodes=Nejsou zatím přiřazeny žádné čárové kódy!
+view.thing.barcode.removed=Čárový kód byl odebrán
+view.thing.barcode.not_removed=Čárový kód nebyl odebrán
+view.thing.barcode.added=Čárový kód byl odstraněn
+view.thing.barcode.not_added=Čárový kód nebyl odstraněn
+view.thing.label.not_linked=Štítek nebyl přiřazen
+view.thing.label.linked=Štítek byl přiřazen
+view.thing.label.not_unlinked=Štítek nebyl odebrán
+view.thing.label.unlinked=Štítek byl odebrán
+view.thing.part.not_added=Část nebyla přidána
+view.thing.part.added=Část byla přidána
+view.thing.part.not_removed=Část nebyla odebrána
+view.thing.part.removed=Část byla odebrána
+
+view.relation.edit=Upravit vztah
+view.relation.to_whole=K celku
+view.relation.to_part=K části
+view.relation.updated=Relace byla aktualizována
+view.relation.not_updated=Relace nebyla aktualizována
+view.relation.deleted=Relace byla odstraněna
 
 view.common.form.create=Vytvořit
 view.common.form.update=Aktualizovat
 view.common.form.delete=Odstranit
+view.common.form.add=Přidat
+view.common.form.remove=Odebrat
 
 view.message.close=Zavřít
 view.message.key.success=Úspěch
@@ -47,3 +87,15 @@ model.thing.description=Popis
 model.thing.since=Od
 model.thing.until=Do
 model.thing.invalid.type=Neznámý typ položky
+
+model.barcode.standard=Standard
+model.barcode.code=Kód
+
+model.relation.part=Část
+model.relation.type=Typ
+model.relation.since=Od
+model.relation.until=Do
+model.relation.slot=Slot
+model.containment.quantity=Počet
+model.component.function=Funkce
+
